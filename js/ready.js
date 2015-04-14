1c4c36c2f4fbbe87612472d60469acad38f8b627
dvdrtrgn <dvdrtrgn@gmail.com>
2015-04-14 18:34:07 -0500

    run a comb thru it

--- js/ready.js
+++ js/ready.js
@@ -40,129 +40,146 @@ if (W.window !== window || !C.debug) {
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 /*globals menuUpdate, skrollr, */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-    var timer,
+$(W.document).ready(function () {
+    var S, location, timer,
+    boxDone = false,
     chapter = 'intro',
     currentId = 'ready',
-    scrollPoint_ready = 3700,
-    scrollPoint_set = 4400,
-    scrollPoint_swing = 5100,
-    scrollPoint_true = 8200,
-    boxDone = false;
-
-$( document ).ready(function() {
-
-    //check if coming from mobile version -- if so, allow
-    var location = window.location.href;
-    if (!location.match(/desktop$/)) {
-
-        var isMobile = {
-            hasTouch: function() {
-                return 'ontouchstart' in document.documentElement;
-            },
-            Android: function() {
-                return navigator.userAgent.match(/Android/i);
-            },
-            BlackBerry: function() {
-                return navigator.userAgent.match(/BlackBerry/i);
-            },
-            iOS: function() {
-                return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-            },
-            Opera: function() {
-                return navigator.userAgent.match(/Opera Mini/i);
-            },
-            Windows: function() {
-                return navigator.userAgent.match(/IEMobile/i);
-            },
-            any: function() {
-                return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-            },
-            ismobi: function() {
-                return navigator.userAgent.match(/Mobi/i);
-            }
-        };
-        if( isMobile.ismobi() ) {
-            window.location="mobile.html";
-        }
-        else {
-            var width = $(window).width(), height = $(window).height();
-            if (width < 640) {
-                window.location="mobile.html";
-            }
-        }
-    }
-
-    else {
-    //display mobile selector
-    //document.getElementById('viewMobile').style.display = "inline";
-    }
-
-
-
-    var s = skrollr.init({
-        smoothScrolling: true,
-        mobileDeceleration: 0.004,
-        forceHeight:true,
-        render: function(data) {
-            //document.querySelector('.scrollerpos').innerHTML = data.curTop;
-            clearTimeout(timer);
-            //scroll position detection
-            if ( ! boxDone ) {
-                if ((chapter != "ready") && (data.curTop >= scrollPoint_ready) && (data.curTop < scrollPoint_set)) {
-                    menuUpdate('ready');
-                }
-                else if ((chapter != "set") && (data.curTop >= scrollPoint_set) && (data.curTop < scrollPoint_swing)) {
-                    menuUpdate('set');
-                }
-                else if ((chapter != "swing") && (data.curTop >= scrollPoint_swing)) {
-                    menuUpdate('swing');
-                }
-
-
-            }
-            else if (boxDone ) {
-                boxDone = false;
-            }
-
-            timer = setTimeout(function() {
-                //The user hasn't scrolled for 1 second.
-                }, 1000);
-        }
-
-
-    });
-    s.animateTo(top);
+    scrollPoints = {
+        ready: 3700,
+        set: 4400,
+        swing: 5100,
+        trew: 8200
+    };
 
     function menuUpdate(chapter) {
         $('#navLeft span').removeClass('active');
-        $('#navLeft #profileButton_'+chapter).addClass('active');
-        var checkElement = $('#navLeft #'+chapter).next();
-        currentId = $('#navLeft #'+chapter).attr('id');
+        $('#navLeft #profileButton_' + chapter).addClass('active');
+        //var checkElement = $('#navLeft #' + chapter).next();
+        currentId = $('#navLeft #' + chapter).attr('id');
     }
 
-    $('#navLeft > span').click(function() {
-        currentId = $(this).attr('id');
-        var result = currentId.slice(14);
-        var chapterPosition = window['scrollPoint_' + result];
-        window.scrollTo(0,chapterPosition);
+    //check if coming from mobile version -- if so, allow
+    location = W.location.href;
+
+    if (!location.match(/desktop$/)) {
+
+        var isMobile = {
+            hasTouch: function () {
+                return 'ontouchstart' in W.document.documentElement;
+            },
+            Android: function () {
+                return W.navigator.userAgent.match(/Android/i);
+            },
+            BlackBerry: function () {
+                return W.navigator.userAgent.match(/BlackBerry/i);
+            },
+            iOS: function () {
+                return W.navigator.userAgent.match(/iPhone|iPad|iPod/i);
+            },
+            Opera: function () {
+                return W.navigator.userAgent.match(/Opera Mini/i);
+            },
+            Windows: function () {
+                return W.navigator.userAgent.match(/IEMobile/i);
+            },
+            any: function () {
+                return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+            },
+            ismobi: function () {
+                return W.navigator.userAgent.match(/Mobi/i);
+            }
+        };
+        if (isMobile.ismobi()) {
+            W.location = 'mobile.html';
+        }
+        else {
+            var width = $(W).width();
+            if (width < 640) {
+                W.location = 'mobile.html';
+            }
+        }
+    }
+    else {
+    /// display mobile selector
+    //W.document.getElementById('viewMobile').style.display = 'inline';
+    }
+
+    W.S = S = skrollr.init({
+        smoothScrolling: true,
+        mobileDeceleration: 0.004,
+        forceHeight: true,
+        render: function (data) {
+            //W.document.querySelector('.scrollerpos').innerHTML = data.curTop;
+            W.clearTimeout(timer);
+            //scroll position detection
+            if (!boxDone) {
+                if ((chapter !== 'ready') && (data.curTop >= scrollPoints.ready) && (data.curTop < scrollPoints.set)) {
+                    menuUpdate('ready');
+                }
+                else if ((chapter !== 'set') && (data.curTop >= scrollPoints.set) && (data.curTop < scrollPoints.swing)) {
+                    menuUpdate('set');
+                }
+                else if ((chapter !== 'swing') && (data.curTop >= scrollPoints.swing)) {
+                    menuUpdate('swing');
+                }
+            }
+            else if (boxDone) {
+                boxDone = false;
+            }
+
+            timer = W.setTimeout(function () {
+                //The user hasn't scrolled for 1 second.
+                }, 1000);
+        }
     });
 
+    S.animateTo('');
+
+    $('#navLeft > span').click(function () {
+        var result, chapterPosition;
+
+        currentId = $(this).attr('id');
+        result = currentId.slice(14);
+        chapterPosition = scrollPoints[result];
+
+        W.scrollTo(0, chapterPosition);
+    });
 
 });
 
-    //$(window).on('resize', function () {
-    //    var width = $(window).width(),
-    //    height = $(window).height();
-    //    if (width < 640) {
-    //    window.location = "mobile.html";
-    //    }
-    //});
-    $(".fitTextIntro").fitText(2.2, { minFontSize: '20px', maxFontSize: '100px' })
-    $(".fittext1").fitText(2.2, { minFontSize: '20px', maxFontSize: '100px' })
-    $(".fittext2").fitText(2.2, { minFontSize: '20px', maxFontSize: '100px' })
-    $(".fittext3").fitText(2.2, { minFontSize: '20px', maxFontSize: '100px' })
-    $(".fittext4").fitText(2.5, { minFontSize: '15px', maxFontSize: '75px' })
-    $(".fittext5").fitText(2.2, { minFontSize: '20px', maxFontSize: '100px' })
-    //$("h1").fitText(2.4, { minFontSize: '20px', maxFontSize: '100px' })
-    //$("h2").fitText(2.4, { minFontSize: '15px', maxFontSize: '75px' })
+//$(window).on('resize', function () {
+//    var width = $(window).width(),
+//    height = $(window).height();
+//    if (width < 640) {
+//    window.location = 'mobile.html';
+//    }
+//});
 
+$('.fitTextIntro').fitText(2.2, {
+    minFontSize: '20px',
+    maxFontSize: '100px'
+});
+$('.fittext1').fitText(2.2, {
+    minFontSize: '20px',
+    maxFontSize: '100px'
+});
+$('.fittext2').fitText(2.2, {
+    minFontSize: '20px',
+    maxFontSize: '100px'
+});
+$('.fittext3').fitText(2.2, {
+    minFontSize: '20px',
+    maxFontSize: '100px'
+});
+$('.fittext4').fitText(2.5, {
+    minFontSize: '15px',
+    maxFontSize: '75px'
+});
+$('.fittext5').fitText(2.2, {
+    minFontSize: '20px',
+    maxFontSize: '100px'
+});
+
+//$('h1').fitText(2.4, { minFontSize: '20px', maxFontSize: '100px' })
+//$('h2').fitText(2.4, { minFontSize: '15px', maxFontSize: '75px' })
