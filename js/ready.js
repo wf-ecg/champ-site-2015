0f6d6f03a84637d0cc0894650117a3899343dbe5
garethlp <garethlp@gmail.com>
2015-04-15 09:39:13 -0400

    Revert "rough in a preloader"
    This reverts commit dd7b39e9944b9daf66a09d46a1d72524198e8e61.

--- js/ready.js
+++ js/ready.js
@@ -146,7 +146,6 @@ $(W.document).ready(function () {
         W.scrollTo(0, chapterPosition);
     });
 
-    $('.preloader').fadeOut(6666); // TODO: find a way to detect load of background images....
 });
 
 //$(window).on('resize', function () {
